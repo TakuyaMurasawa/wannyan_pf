066be7acbcfab96b004f48c5a1fbec7a9ad63cf3
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-08-30 02:18:45 +0000

    [pf-UPDATE]

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -29,11 +29,16 @@ end
   end
 
   def edit
-    @customer = current_customer
+    @customer = Customer.find(params[:id])
   end
 
   def update
-    @customer = current_customer
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
