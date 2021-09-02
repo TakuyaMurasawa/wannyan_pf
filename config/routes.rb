1a86bfc1af7aea860e534e676326dfce8d6bff32
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-09-02 14:16:45 +0000

    [pf-ADD]admin

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  end
 end
