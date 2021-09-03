0f95b3d8a5141b866758746812bddfd84cd4f1df
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-09-03 13:15:25 +0000

    [pf-UPDATE]admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   resources :animals
   resources :messages, only: [:create]
   resources :rooms, only: [:create,:show]
-  
+
   get '/search', to: 'searches#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
+    resources :customers, only: [:new, :destroy, :create]
   end
 end
