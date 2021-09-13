0ad3c7f2979cc625d2ef04404ac3575d628ce0fb
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-09-13 14:19:06 +0000

    [pf-UPDATE]admin/animal_controller

--- app/controllers/admin/animals_controller.rb
+++ app/controllers/admin/animals_controller.rb
@@ -1,10 +1,12 @@
 class Admin::AnimalsController < ApplicationController
   def index
+    @animals = Animal.page(params[:page])
   end
-  
+
   def show
+    @animal = Animal.find(params[:id])
   end
-  
+
   def destroy
   end
 end
