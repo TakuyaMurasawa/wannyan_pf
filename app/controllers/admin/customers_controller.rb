cb1feaa5527eacd1db291d26907e0c0d6926d1ad
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-09-17 14:48:03 +0000

    [pf-UPDATE]admin/cintroller/customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,12 +2,19 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def destroy
-    
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:user_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :prefecture, :city, :house_number,:other, :phone_number, :email )
   end
 end
