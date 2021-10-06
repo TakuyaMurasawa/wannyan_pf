c3511e7434c606679b354ed271090dbfc978b1b4
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-10-06 14:28:58 +0000

    [test-ADD]calculation

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   resources :animals
   resources :messages, only: [:create]
   resources :rooms, only: [:create,:show]
+  
+  resources :calculation, only: [:index, :show]
 
   get '/search', to: 'searches#search'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
