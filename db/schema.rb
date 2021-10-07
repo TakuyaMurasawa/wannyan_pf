c89cca0a71475953a5ee2c94b5a7cb5badae2c97
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-10-07 07:49:31 +0000

    [ADD]model/calculation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_170034) do
+ActiveRecord::Schema.define(version: 2021_10_07_070835) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 2021_08_28_170034) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "animals", force: :cascade do |t|
     t.string "title", null: false
     t.integer "tipe", default: 0, null: false
@@ -49,6 +61,11 @@ ActiveRecord::Schema.define(version: 2021_08_28_170034) do
     t.text "content"
     t.integer "customer_id"
     t.string "image_id"
+  end
+
+  create_table "calculations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
