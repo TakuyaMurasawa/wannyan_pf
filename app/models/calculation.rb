04cc69d31d1df339ea799d0a196bb6956a853213
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-10-14 14:02:31 +0000

    [IPDATE}]

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,11 +1,11 @@
 class Calculation < ApplicationRecord
 
-  # def tribonacci(n)
-  # return   if n < 1
-  # a, b, c = 0, 0, 1
-  # (n - 1).times { a, b, c = b, c, a + b + c }
-  # a
-  # end
+  def tribonacci(n)
+  return   if n < 1
+  a, b, c = 0, 0, 1
+  (n - 1).times { a, b, c = b, c, a + b + c }
+  a
+  end
 
   def tribonacci n
   if n == 1
